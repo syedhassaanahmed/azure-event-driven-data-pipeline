f8e9685674133474220e91ecf3db4d0a119508f3
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2018-03-02 11:05:08 +0100

    Add Service Bus Queue

---
+++ HttpIngressFunc/HttpIngressFunc.cs
@@ -0,0 +1,16 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace HttpIngressFunc
+{
+    public static class HttpIngressFunc
+    {
+        [FunctionName("HttpIngressFunc")]
+        [return: ServiceBus("productsQueue", AccessRights.Send, Connection = "SERVICEBUS_CONNECTION")]
+        public static string Run([HttpTrigger] dynamic product, TraceWriter log)
+        {
+            return product.ToString();
+        }
+    }
+}
